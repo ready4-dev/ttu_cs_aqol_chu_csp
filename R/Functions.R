6ed91f30e58d2189685084507ee2f3ef50c7c698
Matthew Hamilton <Matthew.Hamilton@orygen.org.au>
2023-05-26 20:23:04 +1000

    failing on mixed modelling

--- R/Functions.R
+++ R/Functions.R
@@ -44,81 +44,86 @@ manufacture_Ready4useDyad <- function(x,
     if(!identical(restrict_to_chr, character(0))){
       factor_vars_chr <- intersect(factor_vars_chr, restrict_to_chr) 
     }
-    object_xx <- purrr::map(factor_vars_chr,
-                            ~ paste0(.x,eval(parse(text=paste0("levels(x@ds_tb$",.x,")"))))) %>%
-      stats::setNames(factor_vars_chr)
-    if(what_1L_chr == "factors-d"){
-      lookup_ls <- object_xx 
-    }
-    if(type_1L_chr == "base"){
-      object_xx <- object_xx %>% purrr::map2(indcs_int, ~ .x[.y])
-    }
-    if(type_1L_chr == "dummys"){
-      object_xx <- object_xx %>% purrr::map2(indcs_int, ~ .x[-.y])
-    }
-    if(remove_ctg_1L_lgl){
-      object_xx <- object_xx %>% purrr::map2(names(object_xx), ~ stringr::str_remove(.x,.y))
-    }
-    if(!identical(correspondences_r3, ready4show::ready4show_correspondences())){
-      object_xx <- manufacture.ready4show_correspondences(correspondences_r3, # update to ready4show::manufacture.ready4show_correspondences
-                                                          data_ls = object_xx,
-                                                          flatten_1L_chr = F, # Correct
-                                                          type_1L_chr = "new",
-                                                          what_1L_chr = "names",
-                                                          ...) 
-    }
-    if(flatten_1L_lgl | what_1L_chr %in% c("factors-c","factors-d")){
-      object_xx <- object_xx %>% purrr::flatten_chr()
-    }
-    if(what_1L_chr %in% c("factors-c", "factors-d")){
-      object_xx <- ready4show::ready4show_correspondences(ready4show::make_pt_ready4show_correspondences(old_nms_chr = object_xx, ...))
-    if(what_1L_chr == "factors-d"){
-      object_xx <- ready4use_dictionary(make_pt_ready4use_dictionary(var_nm_chr = object_xx %>% purrr::pmap_chr(~ifelse(is.na(as.character(..2)),
-                                                                                                                        as.character(..1),as.character(..2))),
-                                                                     var_ctg_chr = object_xx$old_nms_chr %>%  purrr::map_chr(~{
-                                                                       level_1L_chr <- .x
-                                                                       names(lookup_ls)[purrr::map_lgl(lookup_ls,~level_1L_chr %in% .x)]
-                                                                     }),
-                                                                     var_desc_chr = lookup_ls %>% 
-                                                                       purrr::map2(names(lookup_ls), 
-                                                                                   ~ stringr::str_remove(.x,.y)) %>% 
-                                                                       purrr::map2(indcs_int, 
-                                                                                   ~ {
-                                                                                     if(!type_1L_chr %in% c("base","dummys")){
-                                                                                       description_chr <- .x
-                                                                                     }
-                                                                                     if(type_1L_chr == "base"){
-                                                                                       description_chr <- .x[.y]
-                                                                                     }
-                                                                                     if(type_1L_chr == "dummys"){
-                                                                                       description_chr <- .x[-.y]
-                                                                                     }
-                                                                                     description_chr
-                                                                                   }) %>% purrr::flatten_chr(),
-                                                                     var_type_chr = object_xx$old_nms_chr %>%  
-                                                                       purrr::map(~{
-                                                                         level_1L_chr <- .x
-                                                                         purrr::map2(lookup_ls, indcs_int,
-                                                                                     ~if(level_1L_chr %in% .x[.y]){
-                                                                                       "base"
-                                                                                     }else{
-                                                                                       if(level_1L_chr %in% .x[-.y]){
-                                                                                         "dummy"#rep("dummy", length(.x[-.y]))
-                                                                                       }else{
-                                                                                         NULL
-                                                                                       }
-                                                                                     }) %>% purrr::discard(is.null) %>%
-                                                                           purrr::flatten_chr()
-                                                                       }) %>% purrr::flatten_chr()))
-    }
-      if(!identical(match_1L_chr, character(0))){
-        object_xx <- ready4::get_from_lup_obj(object_xx,
-                                              match_value_xx = match_1L_chr,
-                                              match_var_nm_1L_chr = ifelse(what_1L_chr == "factors-d","var_nm_chr","old_nms_chr"),
-                                              target_var_nm_1L_chr = ifelse(what_1L_chr == "factors-d",target_1L_chr,"new_nms_chr"))
+    if(type_1L_chr == "vars" && what_1L_chr == "factors"){
+      object_xx <- factor_vars_chr 
+    }else{
+      object_xx <- purrr::map(factor_vars_chr,
+                              ~ paste0(.x,eval(parse(text=paste0("levels(x@ds_tb$",.x,")"))))) %>%
+        stats::setNames(factor_vars_chr)
+      if(what_1L_chr == "factors-d"){
+        lookup_ls <- object_xx 
       }
-      
+      if(type_1L_chr == "base"){
+        object_xx <- object_xx %>% purrr::map2(indcs_int, ~ .x[.y])
       }
+      if(type_1L_chr == "dummys"){
+        object_xx <- object_xx %>% purrr::map2(indcs_int, ~ .x[-.y])
+      }
+      if(remove_ctg_1L_lgl){
+        object_xx <- object_xx %>% purrr::map2(names(object_xx), ~ stringr::str_remove(.x,.y))
+      }
+      if(!identical(correspondences_r3, ready4show::ready4show_correspondences())){
+        object_xx <- manufacture.ready4show_correspondences(correspondences_r3, # update to ready4show::manufacture.ready4show_correspondences
+                                                            data_ls = object_xx,
+                                                            flatten_1L_chr = F, # Correct
+                                                            type_1L_chr = "new",
+                                                            what_1L_chr = "names",
+                                                            ...) 
+      }
+      if(flatten_1L_lgl | what_1L_chr %in% c("factors-c","factors-d")){
+        object_xx <- object_xx %>% purrr::flatten_chr()
+      }
+      if(what_1L_chr %in% c("factors-c", "factors-d")){
+        object_xx <- ready4show::ready4show_correspondences(ready4show::make_pt_ready4show_correspondences(old_nms_chr = object_xx, ...))
+        if(what_1L_chr == "factors-d"){
+          object_xx <- ready4use_dictionary(make_pt_ready4use_dictionary(var_nm_chr = object_xx %>% purrr::pmap_chr(~ifelse(is.na(as.character(..2)),
+                                                                                                                            as.character(..1),as.character(..2))),
+                                                                         var_ctg_chr = object_xx$old_nms_chr %>%  purrr::map_chr(~{
+                                                                           level_1L_chr <- .x
+                                                                           names(lookup_ls)[purrr::map_lgl(lookup_ls,~level_1L_chr %in% .x)]
+                                                                         }),
+                                                                         var_desc_chr = lookup_ls %>% 
+                                                                           purrr::map2(names(lookup_ls), 
+                                                                                       ~ stringr::str_remove(.x,.y)) %>% 
+                                                                           purrr::map2(indcs_int, 
+                                                                                       ~ {
+                                                                                         if(!type_1L_chr %in% c("base","dummys")){
+                                                                                           description_chr <- .x
+                                                                                         }
+                                                                                         if(type_1L_chr == "base"){
+                                                                                           description_chr <- .x[.y]
+                                                                                         }
+                                                                                         if(type_1L_chr == "dummys"){
+                                                                                           description_chr <- .x[-.y]
+                                                                                         }
+                                                                                         description_chr
+                                                                                       }) %>% purrr::flatten_chr(),
+                                                                         var_type_chr = object_xx$old_nms_chr %>%  
+                                                                           purrr::map(~{
+                                                                             level_1L_chr <- .x
+                                                                             purrr::map2(lookup_ls, indcs_int,
+                                                                                         ~if(level_1L_chr %in% .x[.y]){
+                                                                                           "base"
+                                                                                         }else{
+                                                                                           if(level_1L_chr %in% .x[-.y]){
+                                                                                             "dummy"#rep("dummy", length(.x[-.y]))
+                                                                                           }else{
+                                                                                             NULL
+                                                                                           }
+                                                                                         }) %>% purrr::discard(is.null) %>%
+                                                                               purrr::flatten_chr()
+                                                                           }) %>% purrr::flatten_chr()))
+        }
+        if(!identical(match_1L_chr, character(0))){
+          object_xx <- ready4::get_from_lup_obj(object_xx,
+                                                match_value_xx = match_1L_chr,
+                                                match_var_nm_1L_chr = ifelse(what_1L_chr == "factors-d","var_nm_chr","old_nms_chr"),
+                                                target_var_nm_1L_chr = ifelse(what_1L_chr == "factors-d",target_1L_chr,"new_nms_chr"))
+        }
+        
+      }
+    }
+
   }
   return(object_xx)
 }
