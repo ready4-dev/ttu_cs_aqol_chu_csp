8143751eb4a877fcf4625333b8af61dbde43ef43
matthewphamilton <matthew.hamilton1@monash.edu>
2024-06-26 16:15:08 +1000

    customisations for firewall

---
+++ LOCAL_INSTALL.R
@@ -0,0 +1,10 @@
+# To circumvent firewall restrictions on installing packages from GitHub:
+## 1 Manually clone the repositories of the required github packages
+## 2 Install from local copies using the following commands
+#devtools::install("../cmdstanr")
+devtools::install("../ready4show")
+devtools::install("../ready4use")
+devtools::install("../youthvars")
+devtools::install("../scorz")
+devtools::install("../specific")
+devtools::install("../TTU")
